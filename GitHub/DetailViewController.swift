b20a7831ef15f3b4c990f068d776965b549885d5
Viktoriia Rohozhyna <>
2018-02-21 13:14:38 +0200

    create a new project with a little UI

---
+++ GitHub/DetailViewController.swift
@@ -0,0 +1,26 @@
+//
+//  DetailViewController.swift
+//  GitHub
+//
+//  Created by Viktoriia Rohozhyna on 2/21/18.
+//  Copyright © 2018 Viktoriia Rohozhyna. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    @IBOutlet weak var nameLbl: UILabel!
+    @IBOutlet weak var descriptionsLbl: UILabel!
+    @IBOutlet weak var urlLbl: UILabel!
+    @IBOutlet weak var forksLbl: UILabel!
+    @IBOutlet weak var startLbl: UILabel!
+    @IBOutlet weak var authorLbl: UILabel!
+    @IBOutlet weak var tagsLbl: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    
+}
---
+++ GitHub/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  GitHub
+//
+//  Created by Viktoriia Rohozhyna on 2/21/18.
+//  Copyright © 2018 Viktoriia Rohozhyna. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    
+
+}
+
